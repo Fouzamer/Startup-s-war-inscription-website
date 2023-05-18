d302cff3f0c68f2137473de1364e56683aac8d41
Fouzamer <88554545+Fouzamer@users.noreply.github.com>
2023-05-18 20:56:16 +0200

    first commit

--- app/Sponsors/page.js
+++ app/Sponsors/page.js
@@ -1,7 +1,11 @@
 export function Sponsors() {
     return(   
 <div className=" grid mb-8 border  rounded-lg shadow-sm border-gray-700  md:mb-12 md:grid-cols-2">
-    {/* <figure className="bg-gradient-to-br from-indigo-950 to-transparent flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700">
+    
+</div>
+    )
+}
+/* <figure className="bg-gradient-to-br from-indigo-950 to-transparent flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700">
         <blockquote className=" max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Sponsors n°1</h3>
             <p className="my-4">brev description sur le Sponsor</p>
@@ -13,7 +17,4 @@ export function Sponsors() {
                 <div className="text-sm text-gray-500 dark:text-gray-400">Developer at Open AI</div>
             </div>
         </figcaption>    
-    </figure> */}
-</div>
-    )
-}
+    </figure> */
